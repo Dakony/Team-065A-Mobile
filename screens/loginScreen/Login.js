6cd45c580fc917a580769ac5c15d7255495c028d
peter2c <peter2c@yahoo.com>
2020-08-31 21:42:08 +0100

    stack created.

--- screens/loginScreen/Login.js
+++ screens/loginScreen/Login.js
@@ -1,17 +1,50 @@
-import React, { Component } from "./node_modules/react";
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title,
+} from "native-base";
 
-export class Login extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text> textInComponent </Text>
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Button style={styles.btn}>
+          <Text style={styles.btnText}>Login</Text>
+        </Button>
+        <Button
+          style={[
+            styles.btn,
+            { marginTop: 20, backgroundColor: "#3e8c7d", borderColor: "#fff" },
+          ]}
+        >
+          <Text style={[styles.btnText, { color: "#fff" }]}>Login</Text>
+        </Button>
       </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-});
+    </View>
+  );
+};
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#fff" },
+  btn: {
+    width: 250,
+    height: 55,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 28,
+    alignSelf: "center",
+    backgroundColor: "#fff",
+    borderColor: "#3e8c7d",
+    borderWidth: 2,
+  },
+  btnText: { color: "#3e8c7d", fontSize: 16, fontWeight: "bold" },
+});
